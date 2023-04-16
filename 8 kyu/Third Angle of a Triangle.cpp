68556fe9348ff3ce54a3fdc9cd6b03e0dbb20fc4
Maksim Skorobogatov <57582106+makskorobogat@users.noreply.github.com>
2023-04-16 15:34:19 +0200

    Update Third Angle of a Triangle.cpp

--- 8 kyu/Third Angle of a Triangle.cpp
+++ 8 kyu/Third Angle of a Triangle.cpp
@@ -1,9 +1,9 @@
 /*
-All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+You are given two interior angles (in degrees) of a triangle.
 
-Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+Write a function to return the 3rd.
 
-Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+Note: only positive integers will be tested.
 */
 
 class Triangle {
